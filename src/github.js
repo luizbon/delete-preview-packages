368faae0dbee3b5b0f14e27d2c0d54ec565a42fb
Luiz Bon <luiz.bon@prospa.com>
2021-08-14 16:33:55 +1000

    Build

--- src/github.js
+++ src/github.js
@@ -1,59 +1,64 @@
-import { Octokit } from "@octokit/rest";
-import semver from "semver";
+import { Octokit } from '@octokit/rest';
+import semver from 'semver';
 
 async function getAllPackages(params) {
-    const octokit = new Octokit({ auth: params.githubToken });
-    const promises = params.packages.map(async (packageName) => {
-        try {
-            const data = await octokit.paginate(
-                octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
-                {
-                    package_type: params.packageType,
-                    org: params.org,
-                    package_name: packageName,
-                    per_page: 100,
-                },
-                response => response.data
-            );
-            const versions = data
-                .map(package => ({ id: package.id, name: package.name }))
-                .sort(({ name: a }, { name: b }) => semver.rcompare(a, b));
-            const currentVersion = versions.find(({ name }) => semver.prerelease(name) === null);
+  const octokit = new Octokit({ auth: params.githubToken });
+  const promises = params.packages.map(async packageName => {
+    try {
+      const data = await octokit.paginate(
+        octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
+        {
+          package_type: params.packageType,
+          org: params.org,
+          package_name: packageName,
+          per_page: 100
+        },
+        response => response.data
+      );
+      const versions = data
+        .map(pkg => ({ id: pkg.id, name: pkg.name }))
+        .sort(({ name: a }, { name: b }) => semver.rcompare(a, b));
+      const currentVersion = versions.find(
+        ({ name }) => semver.prerelease(name) === null
+      );
 
-            return versions.filter(({ name }) => semver.prerelease(name) && semver.lt(name, currentVersion.name));
-        } catch (error) {
-            return [];
-        }
-    });
-    const resp = await Promise.all(promises);
-    return params.packages.reduce((accumulator, currentValue, index) => {
-        accumulator[currentValue] = resp[index];
-        return accumulator;
-    });
+      return versions.filter(
+        ({ name }) =>
+          semver.prerelease(name) && semver.lt(name, currentVersion.name)
+      );
+    } catch (error) {
+      return [];
+    }
+  });
+  const resp = await Promise.all(promises);
+  return params.packages.reduce((accumulator, currentValue, index) => {
+    accumulator[currentValue] = resp[index];
+    return accumulator;
+  });
 }
 
 export async function deletePackages(params) {
-    const packages = await getAllPackages(params);
-    if (params.dryRun) {
-        Object.keys(packages).forEach(async (packageName) => {
-            console.log(`DRY RUN: would delete ${packageName}`);
-            console.log(packages[packageName]);
-        });
-        return;
-    }
-
-    const octokit = new Octokit({ auth: params.githubToken });
-
-    const deletePromises = Object.keys(packages).map(async (packageName) => {
-        return packages[packageName].map(async (version) => {
-            return octokit.packages.deletePackageVersionForOrg({
-                package_type: params.packageType,
-                package_version_id: version.id,
-                package_name: packageName,
-                org: params.org,
-            });
-        });
+  const packages = await getAllPackages(params);
+  if (params.dryRun) {
+    Object.keys(packages).forEach(async packageName => {
+      console.log(`DRY RUN: would delete ${packageName}`);
+      console.log(packages[packageName]);
     });
+    return;
+  }
 
-    await Promise.all(deletePromises);
+  const octokit = new Octokit({ auth: params.githubToken });
+
+  const deletePromises = Object.keys(packages).map(async packageName => {
+    return packages[packageName].map(async version => {
+      return octokit.packages.deletePackageVersionForOrg({
+        package_type: params.packageType,
+        package_version_id: version.id,
+        package_name: packageName,
+        org: params.org
+      });
+    });
+  });
+
+  await Promise.all(deletePromises);
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,34 @@
-import { getInput, getMultilineInput, setFailed, setOutput } from "@actions/core";
+import {
+  getInput,
+  getMultilineInput,
+  setFailed,
+  setOutput
+} from '@actions/core';
 import { deletePackages } from './github';
 
 async function run() {
-    console.log('run action');
-    try {
-        const githubToken = getInput('github_token');
-        const owner = getInput('owner');
-        const repo = getInput('repo');
-        const packages = getMultilineInput('packages');
-        const packageType = getInput('package_type');
-        const dryRun = getInput('dry_run');
+  console.log('run action');
+  try {
+    const githubToken = getInput('github_token');
+    const owner = getInput('owner');
+    const repo = getInput('repo');
+    const packages = getMultilineInput('packages');
+    const packageType = getInput('package_type');
+    const dryRun = getInput('dry_run');
 
-        await deletePackages({
-            githubToken,
-            owner,
-            repo,
-            packages,
-            packageType,
-            dryRun
-        });
+    await deletePackages({
+      githubToken,
+      owner,
+      repo,
+      packages,
+      packageType,
+      dryRun
+    });
 
-        setOutput('time', new Date().toTimeString());
-    }
-    catch (err) {
-        setFailed(err.message);
-    }
+    setOutput('time', new Date().toTimeString());
+  } catch (err) {
+    setFailed(err.message);
+  }
 }
 
 run();
